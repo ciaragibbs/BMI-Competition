01707ec7b87102ba92b04e690b20b9ef66bc6581
jediknightnapoleon <44095714+jediknightnapoleon@users.noreply.github.com>
2022-03-02 21:30:29 +0000

    Create find_velocity.m
    Added a function to find velocity at each time step

---
+++ Preprocessing/find_velocity.m
@@ -0,0 +1,19 @@
+function trial_with_v = find_velocity(trial)
+% This function calculates the 'velocity' (i.e. position difference between neighbouring time steps) for a dataset struct, it will return 
+% a new struct with a velocity attribute (? not sure about the terminology but basically trial.velocity). Moreover, it will also calculate
+% the direction in x-y plane (angle) and the magnitude (speed)
+trial_with_v = trial;
+
+for i = 1: size(trial,1)
+
+        for j = 1:size(trial,2)
+            % Calculate difference between every timestep's handpos, atm the velocity for the last step will be 0, can choose to ignore
+            trial_with_v(i,j).velocity = [diff(trial(i,j).handPos,1,2), zeros(size(trial(i,j).handPos,1),1)]; 
+            trial_with_v(i,j).angle = atan2(trial_with_v(i,j).velocity(2,:),trial_with_v(i,j).velocity(1,:));
+            trial_with_v(i,j).speed = sqrt(sum(trial_with_v(i,j).velocity.^2, 1));
+        end
+        
+end
+
+end
+
