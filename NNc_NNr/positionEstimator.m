5c22203ed009c38898969a75a861ab6fe5bdec80
fo1318 <72918770+fo1318@users.noreply.github.com>
2022-03-15 09:49:27 +0000

    Update positionEstimator.m

--- NNc_NNr/positionEstimator.m
+++ NNc_NNr/positionEstimator.m
@@ -1,9 +1,22 @@
 function [decodedPosX, decodedPosY] = positionEstimator(past_current_trial, modelParameters)
-    dt = 5;
+%     Get direction of the trajectory
+    netc = modelParameters{9};
+    t_ = round(size(past_current_trial.spikes,2)/2);
+    t = t_+1:t_:size(past_current_trial.spikes,2);
+    spikes_num = [];
+    for i=t
+       spikes_num = [spikes_num, sum(past_current_trial.spikes(:,i-t_:i),2)];
+    end
+    y = netc(spikes_num);
+    
+    [~,direc] = max(y);
+    netr = modelParameters{direc};
+
+    dt = 20;
     input_val = preprocess_input(past_current_trial.spikes,dt);
     t = 0:dt:size(past_current_trial.spikes,2);
 
-    pred = modelParameters(input_val');
+    pred = netr(input_val');
 
     interp_predx = interp1(t(1:end-1),pred(1,:),0:size(past_current_trial.spikes,2),'linear');
     interp_predy = interp1(t(1:end-1),pred(2,:),0:size(past_current_trial.spikes,2),'linear');
